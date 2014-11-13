2e92c021f009d4084fe2ff0348a0d1db36a5ccaa
peter miller <petermiller1986@gmail.com>
2014-11-13 17:40:08 +1030

    keep to column limit

--- progress_meter.py
+++ progress_meter.py
@@ -9,7 +9,8 @@ def render(percent, extra_text = ""):
 			percent = 0
 		if percent > 100:
 			percent = 100
-		text = "%06.2f%% [%s] %s" % (percent, ("#" * int(percent)).ljust(100), extra_text)
+		text = "%06.2f%% [%s] %s" \
+		% (percent, ("#" * int(percent)).ljust(100), extra_text)
 	else: # assume percent is not defined then 
 		text = extra_text
 	if previous_text == text:
@@ -20,7 +21,8 @@ def render(percent, extra_text = ""):
 
 def done():
 	global previous_text
-	sys.stdout.write("\n") # note that print("\n") does not work - it prints two newlines
+	# note that print("\n") does not work - it prints two newlines
+	sys.stdout.write("\n")
 	sys.stdout.flush()
 	previous_text = ""
 
