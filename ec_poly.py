2e53cc37c1d6573ea9afa37875609a07854cd100
peter <petermiller1986@gmail.com>
2015-04-06 12:31:38 +0930

    run as standalone script. merge equation-as-strings into calculation functions.

--- ec_poly.py
+++ ec_poly.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python2.7
+
 """
 functions related to plotting and calculating points on bitcoin's ecdsa
 polynomial (secp256k1): y^2 = x^3 + 7
+
+this file is standalone - it is just for understanding concepts, not used for
+calculating real public or private keys.
 """
 
 import numpy as np
@@ -15,97 +20,155 @@ def find_ints_on_curve(max_x):
 		y_int = int(y)
 		recalc_mid_x = y_int**2
 		if recalc_mid_x == mid_x:
-		#if y_int == y: # don't use this - it is not correct
+		#if y_int == y: # don't use this - it fails due to rounding errors
 			print "x = ", x, "y = ", y
 		x += 1
 
-def y(xp, pos = True):
-	y = (xp**3 + 7)**0.5
-	return y if pos else -y
-
-def yr3(xr3, xp, yp, m):
+def y_ec(xp, pos = True, string = False):
 	"""
-	the line intersects the curve on its 3rd root - the other two are known.
-	for point doubling there is a double root at x = xp, or for point addition
-	there is a root at x = xp and a root at x = xq.
+	return either the value of y at point x = xp, or the equation for y in terms
+	of xp
 	"""
-	return m * (xr3 - xp) + yp
+	if string:
+		return "%s(%s^3 + 7)^0.5" % ("" if pos else "-", brackets(xp))
+	else:
+		y = (xp**3 + 7)**0.5
+		return y if pos else -y
 
-def yr3_str(xr3, xp, yp, m):
-	return "(%s)(%s - %s) + %s" % (m, xr3, xp, yp)
-
-def y_str(x):
-	return "(%s^3 + 7)^0.5" % x
-
-def tan_slope(x):
-	"""calculate the slope of the tangent to curve y^2 = x^3 + 7 at x"""
-	return (3 * x**2) / (2 * (x**3 + 7)**0.5)
-
-def tan_slope_str(x):
-	return "3%s^2 / (2(%s^3 + 7)^0.5)" % (x, x)
-
-def non_tan_slope(xp, xq):
+def y_line(xr, xp, yp, m, string = False):
 	"""
-	calculate the slope of the line that passes through p and q on curve
-	y^2 = x^3 + 7
+	return either the value of y at point x = xr, or the line equation for y in
+	terms of xr
 	"""
-	return (y(xp) - y(xq))/(xp - xq)
+	if string:
+		return "%s(%s - %s) + %s" % (
+			brackets(m), brackets(xr), brackets(xp), brackets(yp)
+		)
+	else:
+		return m * (xr - xp) + yp
 
-def non_tan_slope_str(xp, xq):
-	yp = y_str(xp)
-	yq = y_str(xq)
-	return "(%s - %s)/(%s - %s)" % (yp, yq, xp, xq)
-
-def slope_str(xp, xq):
+def slope(xp, xq, string = False):
 	"""return the equation of the slope which passes through points xp and xq"""
 	if xp == xq:
 		# when both points are on top of each other then we need to find the
 		# tangent slope (the differential at xp)
-		return tan_slope_str(xp)
+		return tan_slope(xp, string = string)
 	else:
-		return non_tan_slope_str(xp, xq)
+		return non_tan_slope(xp, xq, string = string)
 
-def line_str(xp, xq):
-	"""return the equation of the line which passes through points xp and xq"""
-	m_str = slope_str(xp, xq)
-	yp = y_str(xp)
-	return "y = (%s)(x - %s) + %s" % (m_str, xp, yp)
+def tan_slope(xp, string = False):
+	"""calculate the slope of the tangent to curve y^2 = x^3 + 7 at xp"""
+	if string:
+		xp = brackets(xp)
+		return "3(%s^2) / (2(%s))" % (xp, y_ec(xp, string = True))
+	else:
+		return (3 * xp**2) / (2 * (xp**3 + 7)**0.5)
 
-def intersection(xp, xq):
+def non_tan_slope(xp, xq, string = False):
+	"""
+	calculate the slope of the line that passes through p and q on curve
+	y^2 = x^3 + 7
+	"""
+	if string:
+		yp = y_ec(xp, string = True)
+		yq = y_ec(xq, string = True)
+		return "(%s - %s)/(%s - %s)" % (yp, yq, brackets(xp), brackets(xq))
+	else:
+		return (y_ec(xp) - y_ec(xq))/(xp - xq)
+
+def intersection(xp, xq, string = False):
 	"""
 	calculate the intersection coordinates of the line through x = xp and x = xq
 	with the curve
 	"""
-	yp = y(xp)
-	if xp == xq:
-		m = tan_slope(xp)
-		xr3 = m**2 - (2 * xp)
-	else:
-		m = non_tan_slope(xp, xq)
-		xr3 = m**2 - xp - xq
-	return (xr3, yr3(xr3, xp, yp, m))
+	yp = y_ec(xp, string = string)
+	m = slope(xp, xq, string = string)
+	xr3 = x_third_root(m, xp, xq, string = string)
+	return (xr3, y_line(xr3, xp, yp, m, string = string))
 
-def intersection_str(xp, xq):
-	"""
-	return the equations for the intersection coordinates of the line through
-	x = xp and x = xq with the curve
-	"""
-	yp = y_str(xp)
-	if xp == xq:
-		m_str = tan_slope_str(xp)
-		xr3_str = "(%s)^2 - 2(%s)" % (m_str, xp)
+def x_third_root(m, xp, xq, string = False):
+	if string:
+		if xp == xq:
+			# simplify a bit
+			return "(%s)^2 - 2(%s)" % (m, xp)
+		else:
+			return "(%s)^2 - %s - %s" % (m, xp, xq)
 	else:
-		m_str = non_tan_slope_str(xp, xq)
-		xr3_str = "(%s)^2 - %s - %s" % (m_str, xp, xq)
-	return (xr3_str, yr3_str(xr3_str, xp, yp, m_str))
+		return m**2 - xp - xq
 
-def plot_ec():
+def init_plot_ec():
+	"""
+	initialize the eliptic curve plot - create the figure and plot the curve but
+	do not put any multiplication lines on it yet and don't show it yet
+	"""
+	global plt
 	# the smallest x value on the curve is -cuberoot(7)
 	x_min = -(7**(1 / 3.0))
-	x = np.arange(x_min, 4, 0.05)
-	y_ = y(x)
+	x_max = 4
+	x = np.arange(x_min, x_max, 0.05)
+	y_ = y_ec(x)
 	plt.figure()
-	plt.plot(x, y_, "b")
-	plt.plot(x, -y_, "b")
+	curve_color = "b" # blue
+	plt.plot(x, y_, curve_color)
+	plt.plot(x, -y_, curve_color)
 	plt.grid(True)
+
+def finalize_plot_ec():
+	global plt
 	plt.show()
+
+def brackets(x):
+	"""
+	put brackets around the variable to make it unambiguous when substituting
+	into string representations of equations
+	"""
+	if (
+		isinstance(x, str) and
+		len(x) > 1
+	):
+		x = "(%s)" % x
+	return x
+
+if __name__ == "__main__":
+	print
+	print """the intersection of the tangent line at x = 1 (positive y) with the curve in
+non-reduced form"""
+	xp = 1
+	xq = xp
+	print intersection(xp, xq, string = True)
+	print """
+--------------------------------------------------------------------------------
+"""
+
+	print """the intersection of the tangent line at x = 1 (positive y) with the curve in
+reduced form"""
+	xp = 1
+	xq = xp
+	print intersection(xp, xq, string = False)
+	print """
+--------------------------------------------------------------------------------
+"""
+
+	print """the equation of the line which passes through x = 1 (positive y) on the curve
+and its intersection with the curve again"""
+	xp = 1
+	yp = y_ec(xp, string = True)
+	m = slope(xp, xp, string = True)
+	print "y = %s" % y_line("x", xp, yp, m, string = True)
+	print """
+--------------------------------------------------------------------------------
+"""
+
+	print """the equation of the bitcoin eliptic curve"""
+	print "y = %s" % y_ec("x", pos = True, string = True)
+	print "and y = %s" % y_ec("x", pos = False, string = True)
+	print """
+--------------------------------------------------------------------------------
+"""
+
+	print """plot the bitcoin eliptic curve"""
+	init_plot_ec()
+	finalize_plot_ec()
+	print """
+--------------------------------------------------------------------------------
+"""
