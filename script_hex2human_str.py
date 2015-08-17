79931eb67250d5cf4a7b56799fcb41edbe18d5c1
peter miller <petermiller1986@gmail.com>
2015-08-18 00:20:15 +0930

    typos

--- script_hex2human_str.py
+++ script_hex2human_str.py
@@ -2,7 +2,7 @@
 
 import os, sys
 
-# when executing this test directly include the parent dir in the path
+# when executing this script directly include the parent dir in the path
 if (
 	(__name__ == "__main__") and
 	(__package__ is None)
@@ -10,14 +10,13 @@ if (
 	os.sys.path.append(
 		os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	)
-
 import btc_grunt
 
 if len(sys.argv) < 2:
 	raise ValueError(
-		"\n\nUsage: ./script_hex2human_str <the script in hex>\n"
-		"eg: ./script_hex2human_str 76a914b0c1c1de86419f7c6f3186935e6bd6ccb52b8"
-		"ee588ac\n\n"
+		"\n\nUsage: ./script_hex2human_str.py <the script in hex>\n"
+		"eg: ./script_hex2human_str.py 76a914b0c1c1de86419f7c6f3186935e6bd6ccb5"
+		"2b8ee588ac\n\n"
 	)
 
 script_hex = sys.argv[1]
