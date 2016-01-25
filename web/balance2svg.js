2fce6abaadedc3dd56538c3ca1aab20940a25bab
peter miller <petermiller1986@gmail.com>
2016-01-26 08:30:07 +1030

    position the vertical and horizontal dashlines using the same function, to avoid code duplication

--- web/balance2svg.js
+++ web/balance2svg.js
@@ -1,53 +1,71 @@
 function balance2svg(balance_array) {
 	svgdoc = document.getElementById('chart-frame').contentDocument;
 	graph_area = svgdoc.getElementById('graph-area');
-	//var graph_area_coordinates = graph_area.getBoundingClientRect();
 	var tttarget = svgdoc.getElementById('tttarget');
-	set_x_axis_values(150, 750);
-	set_y_axis_values(0, 20);
+	set_axis_values('x', 150, 750);
+	set_axis_values('y', 0, 20);
+
+	set_axis_values('x', 0, 800);
+	set_axis_values('y', 0, 80);
+
+
 	tttarget.setAttribute('cx', 100);
-	/*
-	rect.setAttribute('x', 55);
-	rect.setAttribute('y', 55);
-	rect.setAttribute('width', 500);
-	rect.setAttribute('height', 500);
-	rect.setAttribute('fill','#95B3D7');
-	svg.appendChild(rect);
-	*/
 }
 //the axes have fixed gridlines, we just need to alter the value at each line
-function set_x_axis_values(lowval, highval) {
+function set_axis_values(x_or_y, lowval, highval) {
 	//use 0 <= x <= 840. beyond this is out of bounds
-	//divide the axis into 8 points with x = 0 at the lowval and x = 840 at the
-	//highval
-	var a_vertical_dashline = svgdoc.getElementById('vertical-dashline0');
-	var display_dashlines = 8;
-	var spacing = 840 / (display_dashlines + 1);
-	var x = 0; //init
-	for(i = 0; i <= display_dashlines; i++) {
-		x += spacing;
-		var newelement = a_vertical_dashline.cloneNode(true);
-		graph_area.appendChild(newelement);
-		pos_absolutely(newelement, x, null);
+	var num_dashlines = 6;
+	switch(x_or_y) {
+		case 'x':
+			var id_str_start = 'vertical-dashline';
+			var svg_chart_max = 840;
+			break;
+		case 'y':
+			var id_str_start = 'horizontal-dashline';
+			var svg_chart_max = 340;
+			break;
+	}
+	var this_id_num = 0; //init
+	var this_id_str = id_str_start + this_id_num;
+	var dashline0 = svgdoc.getElementById(this_id_str);
+	var position_spacing = svg_chart_max / (num_dashlines + 1);
+	var display_value = lowval; //init
+	dashline0.children[0].textContent = one_dp(display_value);
+	var display_spacing = (highval - lowval) / (num_dashlines + 1);
+	var position = 0; //init
+	for(i = 0; i <= num_dashlines; i++) {
+		this_id_num = i + 1;
+		this_id_str = id_str_start + this_id_num;
+		position += position_spacing;
+		display_value += display_spacing;
+
+		//if the element already exists then re-use it
+		var newelement = svgdoc.getElementById(this_id_str);
+		if(newelement === null) {
+			var newelement = dashline0.cloneNode(true);
+			graph_area.appendChild(newelement);
+		}
+		newelement = set_id(newelement, this_id_str);
+		newelement.children[0].textContent = one_dp(display_value);
+		switch(x_or_y) {
+			case 'x': position_absolutely(newelement, position, null); break;
+			case 'y': position_absolutely(newelement, null, position); break;
+		}
 	}
 }
-function set_y_axis_values(lowval, highval) {
-	//use 0 <= y <= 340. beyond this is out of bounds
-	var a_horizontal_dashline = svgdoc.getElementById('horizontal-dashline0');
-	a_horizontal_dashline.children[0].textContent = 'qwe';
-	var display_dashlines = 8;
-	var spacing = 340 / (display_dashlines + 1);
-	var y = 0; //init
-	for(i = 0; i <= display_dashlines; i++) {
-		y += spacing;
-		var newelement = a_horizontal_dashline.cloneNode(true);
-		graph_area.appendChild(newelement);
-		pos_absolutely(newelement, null, y);
-	}
+function one_dp(val) {
+	//round number to 1 decimal place
+	return Math.round(val * 10) / 10;
 }
-function pos_absolutely(el, x, y)
-{
+function position_absolutely(el, x, y) {
 	var matrix = el.transform.baseVal.getItem(0).matrix;
 	if(x !== null) matrix.e = x;
 	if(y !== null) matrix.f = y;
+}
+function set_id(el, id_str) {
+	//set the id for an element. this gets its own function in case a better way
+	//of doing it is found in future. setAttribute() is known to be worse
+	//(http://stackoverflow.com/a/4852404/339874)
+	el.id = id_str;
+	return el;
 }
