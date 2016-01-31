de5fa41e0d9e848120ae4cc8841aa03f3101fab1
peter miller <petermiller1986@gmail.com>
2016-02-01 09:11:52 +1030

    convert satoshis to other currencies

---
+++ web/currency_convert.js
@@ -0,0 +1,14 @@
+function balance_currency_convert(satoshis_json, exchange_rate) {
+	//convert a json list of the format [[time, satoshis], [time, satoshis]]
+	//to the format [time, {'btc': 123, 'sat': 321, 'local': 2}]
+	var balance_json = []
+	for(var i = 0; i < satoshis_json.length; i++) {
+		var satoshis_i = satoshis_json[i][1]
+		balance_json[i] = [satoshis_json[i][0], {
+			'sat': satoshis_i,
+			'btc': satoshis_i / 100000000,
+			'local': satoshis_i * exchange_rate / 100000000
+		}]
+	}
+	return balance_json;
+}
