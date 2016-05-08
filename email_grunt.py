67e300e234dc21dfecbd1773fef08ef4853127ed
peter miller <petermiller1986@gmail.com>
2016-05-09 00:31:10 +0930

    added an email library to send notifications from servers

---
+++ email_grunt.py
@@ -0,0 +1,21 @@
+"""module containing some general email-related functions"""
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send(subject, html):
+    from_address = "user@localhost"
+    to_address = "me@email.com"
+
+    # create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg.attach(MIMEText(html, 'html'))
+
+    # send the message via local SMTP server.
+    s = smtplib.SMTP('localhost')
+    s.sendmail(from_address, to_address, msg.as_string())
+    s.quit()
