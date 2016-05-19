0d4083cb3b0fb7ea96d7fbeb07feb08389b317f3
peter miller <petermiller1986@gmail.com>
2016-05-20 07:11:31 +0930

    write errors to logfile as well as emailing, in case emails fail

---
+++ db_task_manager.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+
+"""manage the tasklist table in the bitcoin db"""
+
+import time, sys, os
+import heartbeat_grunt
+import email_grunt
+import map_addresses_to_txouts
+import filesystem_grunt
+
+logfile = "/tmp/%s.log" % os.path.basename(sys.argv[0])
+prepend_datetime = True
+
+while(True):
+    time.sleep(10) # check every 10 seconds for new tasks
+    try:
+        # put all tasks here. make sure to keep tasks very quick, otherwise this
+        # will not run approx every 10 seconds
+        map_addresses_to_txouts.manage()
+    except Exception as e:
+        email_grunt.send(email_grunt.standard_error_subject, e.msg)
+        filesystem_grunt.update_logfile(logfile, e.msg, prepend_datetime)
+    heartbeat.clear()
--- filesystem_grunt.py
+++ filesystem_grunt.py
@@ -1,6 +1,6 @@
 """module containing some general filesystem-related functions"""
 
-import os, errno
+import os, errno, time
 
 def make_sure_path_exists(path):
     # thanks to http://stackoverflow.com/a/5032238/339874
@@ -9,3 +9,10 @@ def make_sure_path_exists(path):
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
+
+def update_logfile(filename, txt, prepend_datetime):
+    prepended_datetime = "" if not prepend_datetime else "[%s] " % (
+        time.strftime("%Y-%m-%d %H:%M:%S")
+    )
+    with open(filename, "a") as f:
+        f.write("%s%s" % (prepended_datetime, txt))
