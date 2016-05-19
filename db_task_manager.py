6ccc3909acf73ba0885125fd995577db8597a16c
peter miller <petermiller1986@gmail.com>
2016-05-20 07:43:49 +0930

    work in progress

--- db_task_manager.py
+++ db_task_manager.py
@@ -7,8 +7,9 @@ import heartbeat_grunt
 import email_grunt
 import map_addresses_to_txouts
 import filesystem_grunt
+import mysql_grunt
 
-logfile = "/tmp/%s.log" % os.path.basename(sys.argv[0])
+logfile = "/tmp/%s.errlog" % os.path.basename(sys.argv[0])
 prepend_datetime = True
 
 while(True):
@@ -17,7 +18,12 @@ while(True):
         # put all tasks here. make sure to keep tasks very quick, otherwise this
         # will not run approx every 10 seconds
         map_addresses_to_txouts.manage()
+
     except Exception as e:
         email_grunt.send(email_grunt.standard_error_subject, e.msg)
         filesystem_grunt.update_logfile(logfile, e.msg, prepend_datetime)
+        heartbeat.clear()
+        mysql_grunt.disconnect()
+        exit()
+
     heartbeat.clear()
