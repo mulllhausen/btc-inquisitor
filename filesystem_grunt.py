aa55db594277386faec3ec910f046af6c7db35f1
peter <petermiller1986@gmail.com>
2016-05-22 00:32:25 +0930

    define the error log location in the config file

--- filesystem_grunt.py
+++ filesystem_grunt.py
@@ -1,6 +1,7 @@
 """module containing some general filesystem-related functions"""
 
 import os, errno, time
+import config_grunt
 
 def make_sure_path_exists(path):
     # thanks to http://stackoverflow.com/a/5032238/339874
@@ -10,9 +11,9 @@ def make_sure_path_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
-def update_logfile(filename, txt, prepend_datetime):
+def update_errorlog(txt, prepend_datetime = True):
     prepended_datetime = "" if not prepend_datetime else "[%s] " % (
         time.strftime("%Y-%m-%d %H:%M:%S")
     )
-    with open(filename, "a") as f:
+    with open(config_grunt.config_dict["error_logfile"], "a") as f:
         f.write("%s%s" % (prepended_datetime, txt))
