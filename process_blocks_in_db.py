a3c67d06b2e5bdd5ab7c8fafe177ad02fdf4e913
peter <petermiller1986@gmail.com>
2016-07-01 09:32:12 +0930

    update the coinbase txin change. needs testing.

--- process_blocks_in_db.py
+++ process_blocks_in_db.py
@@ -54,20 +54,48 @@ def process_range(block_height_start, block_height_end):
         inner join (
             select sum(funds) as txins_total, tx_hash
             from blockchain_txins
+            where block_height >= %s
+            and block_height < %s
             group by tx_hash
         ) txin on tx.tx_hash = txin.tx_hash
         inner join (
             select sum(funds) as txouts_total, tx_hash
             from blockchain_txouts
+            where block_height >= %s
+            and block_height < %s
             group by tx_hash
         ) txout on tx.tx_hash = txout.tx_hash
         set tx.tx_change = (txin.txins_total - txout.txouts_total)
         where tx.tx_change is null
+        and tx.tx_num != 0
         and tx.block_height >= %s
         and tx.block_height < %s
-    """, (block_height_start, block_height_end))
+    """, (block_height_start, block_height_end) * 3)
     print "done. %d rows updated\n" % mysql_grunt.cursor.rowcount
 
+    # todo - test and debug this
+    print "updating the coinbase change funds for each coinbase txin between" \
+    " block %d and %d..." \
+    % (block_height_start, block_height_end)
+
+    mysql_grunt.cursor.execute("""
+        update blockchain_txins txin
+        inner join (
+            select sum(tx_totals.tx_change) as total_change, tx0.tx_hash
+            from blockchain_txs tx_totals
+            inner join blockchain_txs tx0 on (
+                tx0.block_hash = tx_totals.block_hash
+                and tx0.tx_num = 0
+            )
+            where tx_totals.tx_num != 0
+            and tx_totals.block_height >= %s
+            and tx_totals.block_height < %s
+        ) block on block.tx_hash = txin.tx_hash
+        set txin.txin_coinbase_change_funds = block.total_change
+        where txin.txin_num = 0
+        and txin.txin_coinbase_change_funds is null
+    """, (block_height_start, block_height_end))
+    print "done. %d rows updated\n" % mysql_grunt.cursor.rowcount
 if (
     (os.path.basename(__file__) == "process_blocks_in_db.py") and
     (len(sys.argv) > 2)
